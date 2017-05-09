3c0ff62719dfbd05cdd94f5b1e41a9d04bf96f84
Peeter Piegaze <ppiegaze@adobe.com>
2017-05-09 10:35:38 +0200

    remove modules

--- models/database.js
+++
@@ -1,8 +0,0 @@
-const promise = require('bluebird');
-const options = {promiseLib: promise};
-const pgp = require('pg-promise')(options);
-const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/todo';
-const client = new pgp.Client(connectionString);
-client.connect();
-const query = client.query('CREATE TABLE items(id SERIAL PRIMARY KEY, text VARCHAR(40) not null, complete BOOLEAN)');
-query.on('end', () => { client.end(); });
